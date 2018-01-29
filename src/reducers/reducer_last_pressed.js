e81ecc19fbb6709c411c1b5170288da4231fbf12
Edgar Dewsbery <edewsbery@gmail.com>
2018-01-29 12:21:03 +0000

    set up basic reducer

--- src/reducers/reducer_last_pressed.js
+++ src/reducers/reducer_last_pressed.js
@@ -1,6 +1,9 @@
+export const NUMBER_PRESSED = 'NUMBER_PRESSED';
+export const FUNCTIONAL_PRESSED = 'FUNCTIONAL_PRESSED';
+
 export default (state = null, action) => {
-  return switch(action.type) {
-    case 'BUTTON_PRESSED':
+   switch(action.type) {
+    case NUMBER_PRESSED:
       return action.payload;
     default: return state
   }
