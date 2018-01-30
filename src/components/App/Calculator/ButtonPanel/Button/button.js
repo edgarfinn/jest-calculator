3daef41dc673825f7979b6fa17aca00461257f03
Edgar Dewsbery <edewsbery@gmail.com>
2018-01-30 09:47:29 +0000

    separate buttonpanel and button into individual components and modules

---
+++ src/components/App/Calculator/ButtonPanel/Button/button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './button.css'
+
+export default ({symbol}) => {
+  return (
+    <button className="button">
+      {symbol}
+    </button>
+  )
+}
--- src/components/App/Calculator/ButtonPanel/button_panel.js
+++ src/components/App/Calculator/ButtonPanel/button_panel.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import './button_panel.css';
+import Button from './Button/button'
 
-const Button = ({value, symbol}) => {
-  return (
-    <button className="button">
-      <span>{symbol}</span>
-    </button>
-  )
-}
 
 export default () => {
+
+  const buttonValues = ['7','8','9','÷','4','5','6','x','1','2','3','-','0','.','=','+'];
+
+  const buttonsArray = buttonValues.map((symbol) => {
+    return (
+      <Button
+        symbol={symbol}
+      />
+    )
+  })
+
+
+
   return (
     <div className="button-panel">
-      <Button value='7' symbol='7'/>
-      <Button value='8' symbol='8'/>
-      <Button value='9' symbol='9'/>
-      <Button value='/' symbol='÷'/>
-      <Button value='4' symbol='4'/>
-      <Button value='5' symbol='5'/>
-      <Button value='6' symbol='6'/>
-      <Button value='*' symbol='x'/>
-      <Button value='1' symbol='1'/>
-      <Button value='2' symbol='2'/>
-      <Button value='3' symbol='3'/>
-      <Button value='-' symbol='-'/>
-      <Button value='0' symbol='0'/>
-      <Button value='.' symbol='.'/>
-      <Button value='=' symbol='='/>
-      <Button value='+' symbol='+'/>
+      {buttonsArray}
     </div>
   )
-
 }
+
+/*
+
+<this.renderButton onButtonPress={this.props.onButtonPress} value='7' symbol='7'/>
+
+*/
